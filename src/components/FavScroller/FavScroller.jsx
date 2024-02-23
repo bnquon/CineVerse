3513c1315f2144f4f94454e08903a76f43b90ecb
Brandon Quon <bqa7@sfu.ca>
2024-02-22 22:55:55 -0800

    added use effect

--- src/components/FavScroller/FavScroller.jsx
+++ src/components/FavScroller/FavScroller.jsx
@@ -1,37 +1,37 @@
-import "./FavScroller.css"
-// WILL PASS THROUGH JSON LIST OF "FAVOURITE MOVIES" AND THEN WILL USE REACT MORE FOR IT
-
+import React, { useEffect } from 'react';
+import "./FavScroller.css";
 
 export const FavScroller = () => {
+    useEffect(() => {
+        // This code will run after the component has been mounted
+        const scrollers = document.querySelectorAll(".scroller");
 
-    const scrollers = document.querySelectorAll(".scroller");
-    
-    function addAnimation() {
-        scrollers.forEach((scroller) => {
-            scroller.setAttribute("data-animated", true);
-            
-            const scrollerInner = scroller.querySelector(".scroller_inner");
-            const scrollerContent = Array.from(scrollerInner.children);
-            
-            scrollerContent.forEach((item) => {
-                const duplicateItem = item.cloneNode(true);
-                duplicateItem.setAttribute("aria-hidden", true);
-                scrollerInner.appendChild(duplicateItem);
+        function addAnimation() {
+            scrollers.forEach((scroller) => {
+                scroller.setAttribute("data-animated", true);
+
+                const scrollerInner = scroller.querySelector(".scroller_inner");
+                const scrollerContent = Array.from(scrollerInner.children);
+
+                scrollerContent.forEach((item) => {
+                    const duplicateItem = item.cloneNode(true);
+                    duplicateItem.setAttribute("aria-hidden", true);
+                    scrollerInner.appendChild(duplicateItem);
+                });
             });
-            
-        });
-    }
-    
-    addAnimation();
-        
+        }
+
+        addAnimation();
+    }, []); // The empty dependency array ensures that the effect runs once after the initial render
+
     return (
-    <div className="scroller">
-        <div className="scroller_inner">
-            <img src="https://placehold.co/100x200" alt="" />
-            <img src="https://placehold.co/100x200" alt="" />
-            <img src="https://placehold.co/100x200" alt="" />
-            <img src="https://placehold.co/100x200" alt="" />
+        <div className="scroller">
+            <div className="scroller_inner">
+                <img src="https://placehold.co/100x200" alt="" />
+                <img src="https://placehold.co/100x200" alt="" />
+                <img src="https://placehold.co/100x200" alt="" />
+                <img src="https://placehold.co/100x200" alt="" />
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
