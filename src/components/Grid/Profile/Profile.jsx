fa1b05a828bc29c58b5724fa3f9fed40886b3be0
Brandon Quon <bqa7@sfu.ca>
2024-02-23 17:49:27 -0800

    Changed font family and more styling to the placeholders

--- src/components/Grid/Profile/Profile.jsx
+++ src/components/Grid/Profile/Profile.jsx
@@ -4,7 +4,7 @@ export const Profile = () => {
   return (
     <div className="profile-container">
         <img id='profile-pic' src="https://placehold.co/175x175/black/white" alt=""/>
-        <h2>USERNAME HERE</h2>
+        <h2 id='username'>USERNAME</h2>
     </div>
   )
 }
