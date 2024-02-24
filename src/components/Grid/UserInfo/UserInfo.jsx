4dc5419e534e23555e7fb371103948e135df87d2
Brandon Quon <bqa7@sfu.ca>
2024-02-23 21:07:47 -0800

    Added things for bio and customizable lists

--- src/components/Grid/UserInfo/UserInfo.jsx
+++ src/components/Grid/UserInfo/UserInfo.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 import "./UserInfo.css"
 export const UserInfo = () => {
+
+  const inputElements = document.getElementsByTagName("input");
+
+  for (let i = 0; i < inputElements.length; i++) {
+    inputElements[i].placeholder = "Enter Here...";
+  }
+
   return (
-    <div className='info-container'>UserInfo</div>
+    <div className='info-container'>
+      <div id="bio-container">
+        <h3>Bio</h3>
+        <textarea placeholder='Type your bio here'></textarea>
+      </div>
+
+      <div id="top3">
+        <h3>Top 3 All Time List</h3>
+        <ol>
+          <li><input type="text" /></li>
+          <li><input type="text" /></li>
+          <li><input type="text" /></li>
+        </ol>
+      </div>
+
+      <div id="overrated">
+        <h3>Top 3 Overrated List</h3>
+        <ol>
+          <li><input type="text" /></li>
+          <li><input type="text" /></li>
+          <li><input type="text" /></li>
+        </ol>
+      </div>
+
+    </div>
   )
 }
