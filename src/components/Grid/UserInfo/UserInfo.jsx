c3252368b0ceeca129bac169c9c1d90cfdd33a15
Brandon Quon <bqa7@sfu.ca>
2024-02-23 21:23:25 -0800

    working on styling for user info container

--- src/components/Grid/UserInfo/UserInfo.jsx
+++ src/components/Grid/UserInfo/UserInfo.jsx
@@ -2,12 +2,6 @@ import React from 'react'
 import "./UserInfo.css"
 export const UserInfo = () => {
 
-  const inputElements = document.getElementsByTagName("input");
-
-  for (let i = 0; i < inputElements.length; i++) {
-    inputElements[i].placeholder = "Enter Here...";
-  }
-
   return (
     <div className='info-container'>
       <div id="bio-container">
@@ -18,21 +12,21 @@ export const UserInfo = () => {
       <div id="top3">
         <h3>Top 3 All Time List</h3>
         <ol>
-          <li><input type="text" /></li>
-          <li><input type="text" /></li>
-          <li><input type="text" /></li>
+          <li><input type="text" placeholder = "Enter Here..."/></li>
+          <li><input type="text" placeholder = "Enter Here..."/></li>
+          <li><input type="text" placeholder = "Enter Here..."/></li>
         </ol>
       </div>
 
       <div id="overrated">
         <h3>Top 3 Overrated List</h3>
         <ol>
-          <li><input type="text" /></li>
-          <li><input type="text" /></li>
-          <li><input type="text" /></li>
+          <li><input type="text" placeholder = "Enter Here..."/></li>
+          <li><input type="text" placeholder = "Enter Here..."/></li>
+          <li><input type="text" placeholder = "Enter Here..."/></li>
         </ol>
       </div>
 
     </div>
-  )
+  );
 }
