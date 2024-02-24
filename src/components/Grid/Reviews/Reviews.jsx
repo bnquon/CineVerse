72aa6619a112a106d1c6a1de943042a852cd1a76
Brandon Quon <bqa7@sfu.ca>
2024-02-23 23:45:53 -0800

    Created barebone card design and implementation

--- src/components/Grid/Reviews/Reviews.jsx
+++ src/components/Grid/Reviews/Reviews.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import "./Reviews.css"
+import { Card } from './Card/Card.jsx'
 
 export const Reviews = () => {
+
   return (
-    <div className='review-container'>Reviews</div>
-  )
+    <div className='review-container'>
+      <h1 id="title">Reviews</h1>
+
+        <div className="review-grid">
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+
+    </div>
+  );
 }
