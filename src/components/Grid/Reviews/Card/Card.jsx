17f638c7ddadcfad4fc7fc6123cccbfd4bcb1bda
Brandon Quon <bqa7@sfu.ca>
2024-02-24 14:11:00 -0800

    Basic styling for the review section done, going to work on barebone graph now

--- src/components/Grid/Reviews/Card/Card.jsx
+++ src/components/Grid/Reviews/Card/Card.jsx
@@ -5,7 +5,20 @@ import "./Card.css"
 export const Card = (props) => {
   return (
     <div className='card-container'>
-        <img src={props.poster} alt="" />    
+        <img src={props.poster} alt="" />
+        <span>
+          <h2>float float float float float float</h2>
+          <h3>⭐10/10</h3>
+          <p>float float float float float floatfloat float floatfloat float 
+            floatfloat float floatfloat float floatfloat float floatfloat 
+            float floatfloat float floatfloat float floatfloat float floatfloat 
+            float floatfloat float floatfloat float floatfloat float floatfloat 
+            float floatfloat float floatfloat float floatfloat float floatfloat 
+            float floatfloat float floatfloat float floatfloat float floatfloat 
+            float floatfloat float floatfloat float floatfloat float
+
+          </p>
+        </span>
     </div>
   )
 }
