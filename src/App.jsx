164f1f6c4a7d0eeb4da04dc79bc2f0ccb15bd514
Brandon Quon <bqa7@sfu.ca>
2024-02-23 16:32:05 -0800

    Creating grid for main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header/Header.jsx";
 import { FavScroller } from "./components/FavScroller/FavScroller.jsx";
+import { Grid } from "./components/Grid/Grid.jsx";
 import "./App.css"
 function App() {
 
   return (
     <>
       <Header/>
+      <Grid/>
       <FavScroller/>
     </>
   );
---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Grid.css"
+
+export const Grid = () => {
+  return (
+    <div className="grid-container">
+        
+    </div>
+  )
+}
