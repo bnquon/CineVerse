6aa960a7ae4e6059e7220835e43ba8e96d0adc9a
Brandon Quon <bqa7@sfu.ca>
2024-02-27 22:29:16 -0800

    added user searcha nd movie search to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilm, faUsers, faSearch } from '@fortawesome/free-solid-svg-icons'
 
 // add variable to store username and push through to this div 
 // maybe make home nav element
 // make icon switcher to search for movies or users
 
 export const Header = () => {
+
+    const [searchType, setSearchType] = useState("movie");
+
+    const userClick = () => {
+      setSearchType("user");
+    }
+
+    const movieClick = () => {
+      setSearchType("movie");
+    }
+
   return (
     <div className="nav-container">
         <div className="left">CINEVERSE</div>
         
         <div className="center">
-            ICON HERE
-            <input type="text" className="input" placeholder='Search...' />
+            <FontAwesomeIcon onClick={movieClick} id='searchMovie' icon={faFilm} style={{ opacity: searchType === 'movie' ? "1" : "0.3" }}/>
+            <FontAwesomeIcon onClick={userClick} id='searchUser' icon={faUsers}  style={{ opacity: searchType === 'user' ? "1" : "0.3" }} />
+            <input type="text" className="input" placeholder={searchType === 'movie' ? "Search Movies..." : "Search Users..."} />
+            <FontAwesomeIcon id='searchIcon' icon={faSearch} />
         </div>
         
         <div className="right">USERNAME</div>
