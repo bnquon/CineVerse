45fbfc12e5ed95726fbd4eeac1260aa1357587e7
Brandon Quon <bqa7@sfu.ca>
2024-02-29 10:56:06 -0800

    Sent back data was undefined

--- src/components/CreateUserPage.jsx
+++ src/components/CreateUserPage.jsx
@@ -21,11 +21,12 @@ export const CreateUserPage = () => {
 
             if (response.ok) {
                 const responseData = await response.json(); // Parse response body as JSON
-                const newUser = responseData.username;
-                const newUserID = responseData.userID;
-                sessionStorage.setItem('username', newUser);
-                sessionStorage.setItem('userID', newUserID);
-                console.log(newUser + ' ' + newUserID);
+                console.log(responseData);
+                // const newUser = responseData.username;
+                // const newUserID = responseData.userID;
+                // sessionStorage.setItem('username', newUser);
+                // sessionStorage.setItem('userID', newUserID);
+                // console.log(newUser + ' ' + newUserID);
                 navigate("./user");
             } else {
                 console.error('Failed to create user:', response.statusText);
