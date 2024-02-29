aacb55a2494b97ac6860af0dcc6b5bd3ece9bb5a
Brandon Quon <bqa7@sfu.ca>
2024-02-28 20:25:13 -0800

    idk

---
+++ Server.cjs
@@ -0,0 +1,60 @@
+import { db } from '@vercel/postgres';
+
+const { Pool } = require('pg');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+async function createTables() {
+  const client = await pool.connect();
+
+  try {
+    const createTablesQuery = `
+      CREATE TABLE IF NOT EXISTS users (
+        userID SERIAL PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        bio TEXT 
+      );
+
+      CREATE TABLE IF NOT EXISTS movies (
+        movieID SERIAL PRIMARY KEY,
+        movieName VARCHAR(255) NOT NULL,
+        averageRating FLOAT
+      );
+      
+      CREATE TABLE IF NOT EXISTS reviews (
+        reviewID SERIAL PRIMARY KEY,
+        user_ID INT REFERENCES users(userID) NOT NULL,
+        movie INT REFERENCES movies(movieID) NOT NULL,
+        rating INT NOT NULL,
+        review TEXT
+      );
+
+      CREATE TABLE IF NOT EXISTS favorites (
+        favoriteID SERIAL PRIMARY KEY,
+        user_ID INT REFERENCES users(userID) NOT NULL,
+        favoriteMovieName VARCHAR(255) NOT NULL
+      );
+    `;
+    // Execute the query
+    await client.query(createTablesQuery);
+
+    console.log('Tables created successfully!');
+  } catch (error) {
+    console.error('Error creating tables:', error.message);
+  } finally {
+    // Release the client back to the pool
+    client.release();  
+  }
+}
+createTables();
