13eb19c967af6e88f74bf8ab37418340da254976
Brandon Quon <bqa7@sfu.ca>
2024-02-29 17:18:56 -0800

    added user upload profile picture but no method to store it and added userinfo table

--- src/components/Grid/Profile/Profile.jsx
+++ src/components/Grid/Profile/Profile.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./Profile.css"
+import noPFP from "../../../assets/noPFP.jpg"
+
 export const Profile = (props) => {
+  const uploadedImage = useRef(null);
+  const imageUploader = useRef(null);
+  const handleImageUpload = (e) => {
+    const [file] = e.target.files;
+    if (file) {
+      const reader = new FileReader();
+      const {current} = uploadedImage;
+      current.file = file;
+      reader.onload = (e) => {
+        current.src = e.target.result;
+      }
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="profile-container">
-        <img id='profile-pic' src="https://placehold.co/175x175/black/white" alt=""/>
+        <input type="file" accept='image/*' multiple='false' id="pfp" ref={imageUploader} onChange={handleImageUpload} style={{display: 'none'}} />
+        <div id="image-container" onClick={() => imageUploader.current.click()}>
+          <img ref={uploadedImage} src={noPFP} />
+        </div>
         <h2 id='username'>{props.username}</h2>
     </div>
   )
