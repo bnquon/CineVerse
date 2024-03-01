42551c8a663459daeea8311fcf51e27086dab216
Brandon Quon <bqa7@sfu.ca>
2024-02-29 21:01:14 -0800

    it works but sends a lot of unneccesary  stuff not jsut the data

--- api/getUserReviews.js
+++ api/getUserReviews.js
@@ -5,7 +5,7 @@ export default async function handler(request, response) {
     const client = await db.connect();
     try {
         const listOfReviews = await client.sql`SELECT * FROM reviews WHERE user_ID = ${userID};`;
-        return response.status(200).json({ reviews: listOfReviews});
+        return response.status(200).json({ reviews: listOfReviews.rows});
     } catch (error) {
         console.error('Error creating user:', error);
         return response.status(500).json({ error: 'Internal Server Error' });
