8ad84c446a2a1f8a1eb3762b7a0edd92713e6a70
Brandon Quon <bqa7@sfu.ca>
2024-03-01 21:07:20 -0800

    Not sure whats happening

--- api/getUserFavorites.js
+++ api/getUserFavorites.js
@@ -5,8 +5,7 @@ export default async function handler(request, response) {
     const client = await db.connect();
     try {
         const data = await client.sql`SELECT FROM favorites WHERE user_ID = ${userID};`;
-        const listOfFavorites = data.rows;
-        return response.status(200).json({ listOfFavorites: listOfFavorites});
+        return response.status(200).json({ listOfFavorites: data});
     } catch (error) {
         console.error('Error getting favorites:', error);
         return response.status(500).json({ error: 'Internal Server Error' });
