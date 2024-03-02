42948fd164371a7bb8f2c632efdf8c4a12e977a9
Brandon Quon <bqa7@sfu.ca>
2024-03-01 21:00:02 -0800

    Finished server code

---
+++ api/getUserFavorites.js
@@ -0,0 +1,14 @@
+import { db } from "@vercel/postgres";
+
+export default async function handler(request, response) {
+    const userID = request.query.userID;
+    const client = await db.connect();
+    try {
+        const data = await client.sql`SELECT FROM favorites WHERE user_ID = ${userID};`;
+        const listOfFavorites = (data.rows).Movieposterurl;
+        return response.status(200).json({ listOfFavorites: listOfFavorites});
+    } catch (error) {
+        console.error('Error getting favorites:', error);
+        return response.status(500).json({ error: 'Internal Server Error' });
+    }
+}
