4d7aacbb8788204b23cff5eca65003b802116665
Brandon Quon <bqa7@sfu.ca>
2024-03-01 21:16:15 -0800

    Added console log to check value of turning movie poster urls into array

--- src/components/FavScroller/FavScroller.jsx
+++ src/components/FavScroller/FavScroller.jsx
@@ -14,7 +14,9 @@ export const FavScroller = (props) => {
                 });
                 if (response.ok) {
                     const data = await response.json();
-                    console.log('LIST OF FAVORITES GET CALL: ',data.listOfFavorites);
+                    const temp = Object.values(data.favoriteList);
+                    console.log('Turning JSON into array MOVIEPOSTERURLS', temp);
+                    setFavoriteList(empty => [...empty, ...temp]);
                 } else console.error('Failed to fetch favorite movies: ', response.statusText);
             } catch (error) {
                 console.error('Error fetching favorite movies: ', error.message);
