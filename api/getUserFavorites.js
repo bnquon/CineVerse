297bcd2d53c2256c16b4f2b47c700a87ff413a48
Brandon Quon <bqa7@sfu.ca>
2024-03-01 21:09:37 -0800

    Changing again

--- api/getUserFavorites.js
+++ api/getUserFavorites.js
@@ -5,7 +5,8 @@ export default async function handler(request, response) {
     const client = await db.connect();
     try {
         const data = await client.sql`SELECT * FROM favorites WHERE user_ID = ${userID};`;
-        return response.status(200).json({ listOfFavorites: data});
+        const listOfFavorites = data.rows;
+        return response.status(200).json({ listOfFavorites: listOfFavorites});
     } catch (error) {
         console.error('Error getting favorites:', error);
         return response.status(500).json({ error: 'Internal Server Error' });
