7d54d1371de9c25215a3168eed22a811341e406a
Brandon Quon <bqa7@sfu.ca>
2024-03-01 21:20:58 -0800

    Trying to update favoriteList

--- src/components/FavScroller/FavScroller.jsx
+++ src/components/FavScroller/FavScroller.jsx
@@ -14,9 +14,10 @@ export const FavScroller = (props) => {
                 });
                 if (response.ok) {
                     const data = await response.json();
-                    const temp = Object.values(data.favoriteList);
-                    console.log('Turning JSON into array MOVIEPOSTERURLS', temp);
-                    setFavoriteList(empty => [...empty, ...temp]);
+                    const temp = data.favoriteList;
+                    const URLarray = Object.values(temp);
+                    console.log('Turning JSON into array MOVIEPOSTERURLS', URLarray);
+                    setFavoriteList(URLarray);
                 } else console.error('Failed to fetch favorite movies: ', response.statusText);
             } catch (error) {
                 console.error('Error fetching favorite movies: ', error.message);
