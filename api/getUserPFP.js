39914fef89319db6f99b19e0cb8dcde68dd141ca
Brandon Quon <bqa7@sfu.ca>
2024-03-03 14:02:49 -0800

    Revert

--- api/getUserPFP.js
+++ api/getUserPFP.js
@@ -5,13 +5,8 @@ export default async function handler(request, response) {
     const client = await db.connect();
 
     try {
-        const existingPFPResult = await client.sql`SELECT ProfilePicture FROM users WHERE userID = ${userID};`;
-        const existingPFP = existingPFPResult[0]?.Profilepicture;
-
-        // Convert Buffer to base64 string
-        const profilePictureBase64 = existingPFP ? existingPFP.toString('base64') : null;
-        
-        return response.status(200).json({ profilePicture: profilePictureBase64 });
+        const existingPFP = await client.sql`SELECT ProfilePicture FROM users WHERE userID = ${userID};`;
+        return response.status(200).json({ profilePicture: existingPFP });
     } catch (error) {
         console.error('Error getting user profile picture:', error);
         return response.status(500).json({ error: 'Internal Server Error' });
