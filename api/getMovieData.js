ac5a678b41115596be7a7357798295482a8949e7
Brandon Quon <bqa7@sfu.ca>
2024-03-03 20:33:56 -0800

    Only sending back rating and review

--- api/getMovieData.js
+++ api/getMovieData.js
@@ -4,7 +4,7 @@ export default async function handler(request, response) {
     const movieTitle = request.query.movieName;
     const client = await db.connect();
     try {
-        const data = await client.sql`SELECT * FROM reviews WHERE movieName = ${movieTitle};`;
+        const data = await client.sql`SELECT rating, review FROM reviews WHERE movieName = ${movieTitle};`;
         return response.status(200).json({ movieInfo: data });
     } catch (error) {
         console.error('Error fetching movie ratings and reviews:', error);
