a58885f591403ebd760edcf11a44bb44fd11eda1
Brandon Quon <bqa7@sfu.ca>
2024-03-03 23:46:30 -0800

    pls

--- src/components/MovieGraph/MovieGraph.jsx
+++ src/components/MovieGraph/MovieGraph.jsx
@@ -1,93 +1,94 @@
-import React, { useState, useEffect } from 'react'
-import "./MovieGraph.css"
+import React, { useState, useEffect } from 'react';
+import "./MovieGraph.css";
 import { Bar } from "react-chartjs-2";
 
 export const MovieGraph = (props) => {
 
+    const [ratings, updateRatings] = useState([]);
+
     useEffect(() => {
-        console.log(props.ratings);     
-    }, []);
+        console.log(props.ratings);
+        const movieRatingsValues = Object.values(props.ratings);
+        updateRatings(movieRatingsValues);
+    }, [props.ratings]);
 
-  return (
-    <>  
-        <div id="left-cell">
-
-            <div id="average">
-                <h2>USER AVERAGE RATING ⭐ 9/10 </h2>
+    return (
+        <>  
+            <div id="left-cell">
+                <div id="average">
+                    <h2>USER AVERAGE RATING ⭐ 9/10 </h2>
+                </div>
+                <div id='graph-Container'>
+                    <Bar 
+                        data={{
+                            labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+                            datasets: [
+                                {
+                                    data: {ratings},
+                                    backgroundColor: 'hsl(17, 83%, 64%)',
+                                    borderWidth: 3,
+                                    borderColor: 'hsl(0, 100%, 50%)',
+                                    barPercentage: 0.7,
+                                },
+                            ],
+                        }}
+                        options={{
+                            scales: {
+                                x: {                        
+                                    ticks: {
+                                        color: 'black', 
+                                        font: {
+                                            size: 18,
+                                            weight: 'bold',
+                                        },
+                                        display: true,
+                                    },
+                                    title: { 
+                                        display: true,
+                                        text: 'Rating',
+                                        color: 'black', 
+                                        font: {
+                                            size: 22,
+                                            weight: 'bold',
+                                        },
+                                    },
+                                    grid: {
+                                        display: false,
+                                    },
+                                    border: {
+                                        display: false,
+                                    },
+                                },
+                                y: {
+                                    title: { 
+                                        display: true,
+                                        text: 'Count',
+                                        color: 'black', 
+                                        font: {
+                                            size: 22,
+                                            weight: 'bold',                               
+                                        },
+                                    },
+                                    ticks: {
+                                        display: false,
+                                    },
+                                    grid: {
+                                        display: false,
+                                    },
+                                    border: {
+                                        display: false,
+                                    },
+                                },
+                            },
+                            plugins: {
+                                legend: {
+                                display: false,
+                                },
+                            }
+                        }}
+                    />
+                </div>
             </div>
-            <div id='graph-Container'>
-                <Bar 
-                    data={{
-                        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-                        datasets: [
-                        {
-                            data: [props.ratings['1'], props.ratings['2'], props.ratings['3'], props.ratings['4'],
-                                    props.ratings['5'], props.ratings['6'], props.ratings['7'], props.ratings['8'], props.ratings['9'], props.ratings['10']],
-                            backgroundColor: 'hsl(17, 83%, 64%)',
-                            borderWidth: 3,
-                            borderColor: 'hsl(0, 100%, 50%)',
-                            barPercentage: 0.7,
-                        },
-                        ],
-                    }}
-                    options={{
-                        scales: {
-                            x: {                        
-                                ticks: {
-                                    color: 'black', 
-                                    font: {
-                                        size: 18,
-                                        weight: 'bold',
-                                    },
-                                    display: true,
-                                },
-                                title: { 
-                                    display: true,
-                                    text: 'Rating',
-                                    color: 'black', 
-                                    font: {
-                                        size: 22,
-                                        weight: 'bold',
-                                    },
-                                },
-                                grid: {
-                                    display: false,
-                                },
-                                border: {
-                                    display: false,
-                                },
-                            },
-                            y: {
-                                title: { 
-                                    display: true,
-                                    text: 'Count',
-                                    color: 'black', 
-                                    font: {
-                                        size: 22,
-                                        weight: 'bold',                               
-                                    },
-                                },
-                                ticks: {
-                                    display: false,
-                                },
-                                grid: {
-                                    display: false,
-                                },
-                                border: {
-                                    display: false,
-                                },
-                            },
-                        },
-                        plugins: {
-                            legend: {
-                            display: false,
-                            },
-                        }
-                    }}
-                />
-            </div>
-        </div>
-    </>
-        
-  )
+        </>
+    );
 }
