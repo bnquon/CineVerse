27fda5dc201cbf0f4c4461f35c8d8c2371a71bf2
Brandon Quon <bqa7@sfu.ca>
2024-03-04 22:12:01 -0800

    Trying to order reviews by most recent so highest to lowest reviewID

--- api/getMovieReviews.js
+++ api/getMovieReviews.js
@@ -5,7 +5,7 @@ export default async function handler(request, response) {
     const client = await db.connect();
 
     try {
-        const data = await client.sql`SELECT review, rating, username FROM reviews WHERE movieName = ${movieName};`;
+        const data = await client.sql`SELECT review, rating, username FROM reviews WHERE movieName = ${movieName} ORDER BY reviewID DESC;`;
         return response.status(200).json({ listOfReviews: data.rows });
     } catch (error) {
         console.error('Error fetching movie reviews:', error);
