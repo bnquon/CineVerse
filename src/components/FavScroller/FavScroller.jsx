647079f0a5835c07ac8035ea59589719dedac20e
Brandon Quon <bqa7@sfu.ca>
2024-03-04 23:07:48 -0800

    Trying to do the favSCroller again

--- src/components/FavScroller/FavScroller.jsx
+++ src/components/FavScroller/FavScroller.jsx
@@ -5,43 +5,40 @@ import placeholder from "../../assets/posterPlaceholder.png"
 export const FavScroller = (props) => {
 
     const [favoriteList, setFavoriteList] = useState([]);
-    const [differenceList, setDifferenceList] = useState([]);
-
-    const populateScroller = async () => {
-        try {
-            const response = await fetch(`/api/getUserFavorites?userID=${props.userID}`, {
-                method: 'GET',
-            });
-            if (response.ok) {
-                const data = await response.json();
-                const listOfFavorites = (data.listOfFavorites).map(item => item.movieposterurl);
-                console.log(listOfFavorites.length);
-
-                let updatedFavorites = [...listOfFavorites];
-
-                if (updatedFavorites.length < 4) {
-                    for (let i = 0; i < (4 - updatedFavorites.length); i++) {
-                        console.log("For loop to push placeholders, i : ", i);
-                        updatedFavorites.push(placeholder);
-                    }
-                    updatedFavorites.push(placeholder);
-                }
-
-                setFavoriteList(updatedFavorites);
-
-            } else console.error('Failed to fetch favorite movies: ', response.statusText);
-        } catch (error) {
-            console.error('Error fetching favoritemovies: ', error.message);
-        }
-    };
 
     useEffect(() => {
-        const fetchDataAndAnimate = async () => {
-            await populateScroller();
-            addAnimation();
+        const populateScroller = async () => {
+            try {
+                const response = await fetch(`/api/getUserFavorites?userID=${props.userID}`, {
+                    method: 'GET',
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    const listOfFavorites = (data.listOfFavorites).map(item => item.movieposterurl);
+                    console.log(listOfFavorites.length);
+
+                    setFavoriteList(listOfFavorites);
+    
+                } else console.error('Failed to fetch favorite movies: ', response.statusText);
+            } catch (error) {
+                console.error('Error fetching favoritemovies: ', error.message);
+            }
+        };
+        populateScroller();
+    }, [])
+
+    useEffect(() => {
+        if (favoriteList.length < 4) {
+            const temp = [...favoriteList];
+            for (let i = 0; i < (4-temp.length); i++) {
+                temp.push(placeholder);
+            }
+            setFavoriteList(temp);
         }
-        fetchDataAndAnimate();
-    }, []);
+        addAnimation();
+    }, [favoriteList])
+
+    
 
     
     function addAnimation() {
@@ -61,17 +58,6 @@ export const FavScroller = (props) => {
         });
     }
 
-    // useEffect(() => {
-    //     if (favoriteList.length < 4) {
-    //         const tempDifferences = [];
-    //         for (let i = 0; i < (4 - favoriteList.length); i++) {
-    //             tempDifferences.push(placeholder);
-    //         }
-    //         setDifferenceList(tempDifferences);
-    //     } else {
-    //         setDifferenceList([]);
-    //     }
-    // }, []);
     
     return (
         <div className="scroller">
@@ -79,12 +65,6 @@ export const FavScroller = (props) => {
                 {favoriteList.map((element, index) => (
                     <img key={index} src={element} alt="" width='175' height='275'/>
                 ))}
-                {/* {favoriteList.map((element, index) => (
-                    <img key={index+2} src={element} alt="" width='175' height='275'/>
-                ))} */}
-                {/* {differenceList.map((element, index) => (
-                    <img key={index + favoriteList.length} src={element}/>
-                ))} */}
             </div>
         </div>
     );
