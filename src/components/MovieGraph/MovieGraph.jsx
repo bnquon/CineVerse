623c8d12865fe603ef3e1415212a5204e84045c9
Brandon Quon <bqa7@sfu.ca>
2024-03-04 18:10:28 -0800

    Hardcoding data for the movie graph to not cause error

--- src/components/MovieGraph/MovieGraph.jsx
+++ src/components/MovieGraph/MovieGraph.jsx
@@ -34,7 +34,7 @@ export const MovieGraph = (props) => {
                         labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
                         datasets: [
                         {
-                            data: movieRatings,
+                            data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                             backgroundColor: 'hsl(17, 83%, 64%)',
                             borderWidth: 3,
                             borderColor: 'hsl(0, 100%, 50%)',
