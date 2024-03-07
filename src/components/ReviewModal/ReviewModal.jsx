4bd1914e4d81cd61885a76c26e5a99dbdcb62054
Brandon Quon <bqa7@sfu.ca>
2024-03-06 22:36:35 -0800

    revert

--- src/components/ReviewModal/ReviewModal.jsx
+++ src/components/ReviewModal/ReviewModal.jsx
@@ -17,16 +17,16 @@ export const ReviewModal = ( {toggleModal} ) => {
             </div>
 
             <div id="modalRating">
-                <div id="1"><FontAwesomeIcon icon={fa1} size='2x' /></div>
-                <div id="2"><FontAwesomeIcon icon={fa2} size='2x'/></div>
-                <div id="3"><FontAwesomeIcon icon={fa3} size='2x'/></div>
-                <div id="4"><FontAwesomeIcon icon={fa4} size='2x'/></div>
-                <div id="5"><FontAwesomeIcon icon={fa5} size='2x'/></div>
-                <div id="6"><FontAwesomeIcon icon={fa6} size='2x'/></div>
-                <div id="7"><FontAwesomeIcon icon={fa7} size='2x'/></div>
-                <div id="8"><FontAwesomeIcon icon={fa8} size='2x'/></div>
-                <div id="9"><FontAwesomeIcon icon={fa9} size='2x'/></div>
-                <div id="10"><FontAwesomeIcon icon={fa1} size='2x'/><FontAwesomeIcon icon={fa0} size='2x'/></div>
+                <div id="1"><FontAwesomeIcon icon={fa1}/></div>
+                <div id="2"><FontAwesomeIcon icon={fa2}/></div>
+                <div id="3"><FontAwesomeIcon icon={fa3}/></div>
+                <div id="4"><FontAwesomeIcon icon={fa4}/></div>
+                <div id="5"><FontAwesomeIcon icon={fa5}/></div>
+                <div id="6"><FontAwesomeIcon icon={fa6}/></div>
+                <div id="7"><FontAwesomeIcon icon={fa7}/></div>
+                <div id="8"><FontAwesomeIcon icon={fa8}/></div>
+                <div id="9"><FontAwesomeIcon icon={fa9}/></div>
+                <div id="10"><FontAwesomeIcon icon={fa1}/><FontAwesomeIcon icon={fa0}/></div>
             </div>
 
             <div id="modalReview">
