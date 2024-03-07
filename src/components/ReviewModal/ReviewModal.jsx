e5aef572c72796a3ae3e65bee1a8babde21516b8
Brandon Quon <bqa7@sfu.ca>
2024-03-06 22:32:17 -0800

    Adjusting size

--- src/components/ReviewModal/ReviewModal.jsx
+++ src/components/ReviewModal/ReviewModal.jsx
@@ -13,24 +13,24 @@ export const ReviewModal = ( {toggleModal} ) => {
         <div className="modal-content">
 
             <div id="modalTitle">
-                <h2>Review Form for MOVIE TITLE</h2>
+                <h2>Creating Review for MOVIE TITLE</h2>
             </div>
 
             <div id="modalRating">
-                <div id="1"><FontAwesomeIcon icon={fa1} /></div>
-                <div id="2"><FontAwesomeIcon icon={fa2} /></div>
-                <div id="3"><FontAwesomeIcon icon={fa3} /></div>
-                <div id="4"><FontAwesomeIcon icon={fa4} /></div>
-                <div id="5"><FontAwesomeIcon icon={fa5} /></div>
-                <div id="6"><FontAwesomeIcon icon={fa6} /></div>
-                <div id="7"><FontAwesomeIcon icon={fa7} /></div>
-                <div id="8"><FontAwesomeIcon icon={fa8} /></div>
-                <div id="9"><FontAwesomeIcon icon={fa9} /></div>
-                <div id="10"><FontAwesomeIcon icon={fa1} /><FontAwesomeIcon icon={fa0} /></div>
+                <div id="1"><FontAwesomeIcon icon={fa1} fa-lg /></div>
+                <div id="2"><FontAwesomeIcon icon={fa2} fa-lg/></div>
+                <div id="3"><FontAwesomeIcon icon={fa3} fa-lg/></div>
+                <div id="4"><FontAwesomeIcon icon={fa4} fa-lg/></div>
+                <div id="5"><FontAwesomeIcon icon={fa5} fa-lg/></div>
+                <div id="6"><FontAwesomeIcon icon={fa6} fa-lg/></div>
+                <div id="7"><FontAwesomeIcon icon={fa7} fa-lg/></div>
+                <div id="8"><FontAwesomeIcon icon={fa8} fa-lg/></div>
+                <div id="9"><FontAwesomeIcon icon={fa9} fa-lg/></div>
+                <div id="10"><FontAwesomeIcon icon={fa1} fa-lg/><FontAwesomeIcon icon={fa0} fa-lg/></div>
             </div>
 
             <div id="modalReview">
-                <h3>Enter Your Review</h3>
+                <h3>Type Your Review</h3>
                 <textarea name="review" id=""></textarea>
             </div>
 
