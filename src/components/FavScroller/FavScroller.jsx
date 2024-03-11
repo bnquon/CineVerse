cac1efa44abb80d6b1315c9cf2b512d4862d7e61
Brandon Quon <bqa7@sfu.ca>
2024-03-10 23:25:14 -0700

    Tryin with placeholders

--- src/components/FavScroller/FavScroller.jsx
+++ src/components/FavScroller/FavScroller.jsx
@@ -75,9 +75,12 @@ export const FavScroller = (props) => {
     return (
         <div className="scroller">
             <div className="scroller__inner">
-                {favoriteList.map((element, index) => (
+                {/* {favoriteList.map((element, index) => (
                     <img key={index} src={element} alt="" width='175' height='100'/>
-                ))}
+                ))} */}
+                <img src="https://placehold.co/300x100/orange/white" alt=""/>
+                <img src="https://placehold.co/300x100/red/orange" alt="" />
+                <img src="https://placehold.co/300x100/black/white" alt=""  />
             </div>
         </div>
     );
