4bd6331fc5309d2f2c03ade131209fa859438070
Brandon Quon <bqa7@sfu.ca>
2024-03-11 23:26:28 -0700

    Adding calendar

--- src/components/Grid/UserInfo/UserInfo.jsx
+++ src/components/Grid/UserInfo/UserInfo.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './UserInfo.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarDay } from '@fortawesome/free-solid-svg-icons'
 
 export const UserInfo = () => {
   const userID = sessionStorage.getItem('userID');
@@ -56,7 +57,7 @@ export const UserInfo = () => {
   return (
     <div className="info-container">
       <div id="date-joined">
-        <h3>Date Joined: INSERT DATE HERE</h3>
+        <span><FontAwesomeIcon icon={faCalendarDay}/><h3>Date Joined: INSERT DATE HERE</h3></span>
       </div>
       <div id="bio-container">
         <h3>Bio</h3>
