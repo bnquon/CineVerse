cf820ab9cacd228ef066c237c4b5066ab4a00346
Brandon Quon <bqa7@sfu.ca>
2024-03-11 22:05:53 -0700

    Styling

--- src/components/CreateUserPage.jsx
+++ src/components/CreateUserPage.jsx
@@ -126,20 +126,38 @@ export const CreateUserPage = () => {
                         <div class="formname">
                             <h1>LOGIN</h1>
                         </div>
-                        <input type="text" name="username" id="loginUsername" placeholder='Username'/>
+                        <div class="inputs">
+                            <input type="text" name="username" id="loginUsername" placeholder='Username'/>
+                            <input type="text" name="password" id="loginPassword" placeholder='Password'/>
+                        </div>
+                        <button id='loginBtn' onClick={loginUser}>Login</button>
+                        <div class="loginnav">
+                            <h3>Don't have an account? </h3>
+                            <button id='gotosignup' onClick={showSignup}>Create User</button>
+                        </div>
+                        {/* <input type="text" name="username" id="loginUsername" placeholder='Username'/>
                         <input type="text" name="password" id="loginPassword" placeholder='Password'/>
                         <button id='loginBtn' onClick={loginUser}>Login</button>
-                        <button id='gotosignup' onClick={showSignup}>Create User</button>
+                        <button id='gotosignup' onClick={showSignup}>Create User</button> */}
                     </div>
 
                     <div id="signup">
                         <div class="formname">
                             <h1>Sign Up</h1>
                         </div>
-                        <input type="text" name="username" id="newUsername" placeholder='New Username'/>
+                        <div class="inputs">
+                            <input type="text" name="username" id="newUsername" placeholder='New Username'/>
+                            <input type="text" name="password" id="newPassword" placeholder='New Password'/>
+                        </div>
+                        <button id="signupBtn" onClick={registerUser}>Sign Up</button>
+                        <div class="loginnav">
+                            <h3>Already have an account? </h3>
+                            <button id="gotologin" onClick={showLogin}>Login</button>
+                        </div>
+                        {/* <input type="text" name="username" id="newUsername" placeholder='New Username'/>
                         <input type="text" name="password" id="newPassword" placeholder='New Password'/>
                         <button id="signupBtn" onClick={registerUser}>Sign Up</button>
-                        <button id="gotologin" onClick={showLogin}>Login</button>
+                        <button id="gotologin" onClick={showLogin}>Login</button> */}
                     </div>
 
                 </div>
