498eeead47794671e0ff348282cb5af242cb43a6
Brandon Quon <bqa7@sfu.ca>
2024-03-12 21:08:02 -0700

    Working on the grid

--- src/components/Grid/UserInfo/UserInfo.jsx
+++ src/components/Grid/UserInfo/UserInfo.jsx
@@ -75,9 +75,9 @@ export const UserInfo = () => {
       <div id="watchlist">
         <h3>Watchlist</h3>
         <div id="watchlistGrid">
-          <img src={placeholder} alt="" />
-          <img src={placeholder} alt="" />
-          <img src={placeholder} alt="" />
+          <div className="tempGridItem"><img src={placeholder} alt="" /></div>
+          <div className="tempGridItem"><img src={placeholder} alt="" /></div>
+          <div className="tempGridItem"><img src={placeholder} alt="" /></div>
         </div>
       </div>
 
