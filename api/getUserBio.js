c9be5d7ed2be065ea5c553cbc61d56b307d0c908
Brandon Quon <bqa7@sfu.ca>
2024-03-13 23:32:09 -0700

    Changing code

--- api/getUserBio.js
+++ api/getUserBio.js
@@ -6,7 +6,9 @@ export default async function handler(request, response) {
 
     try {
         const existingBio = await client.sql`SELECT * FROM userInfo WHERE userID = ${userID};`;
-        const sendBio = (existingBio.rows[0]).bio;
+        var sendBio;
+        if (existingBio) sendBio = (existingBio.rows[0]).bio;
+        
         const temp = await client.sql`SELECT movieposterURL FROM watchlist WHERE userID = ${userID};`;
         const watchlist = temp.rows;
         return response.status(200).json({ bio: sendBio, savedWatchlist: watchlist });
