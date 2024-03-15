370de1ed709dacec36d97e34b73b4afa210e003e
Brandon Quon <bqa7@sfu.ca>
2024-03-15 11:44:05 -0700

    altering fav scroller

--- src/components/FavScroller/FavScroller.jsx
+++ src/components/FavScroller/FavScroller.jsx
@@ -7,7 +7,6 @@ export const FavScroller = (props) => {
     const [favoriteList, setFavoriteList] = useState([]);
 
     useEffect(() => {
-        setFavoriteList([]);
         const populateScroller = async () => {
             try {
                 const response = await fetch(`/api/getUserFavorites?userID=${props.userID}`, {
@@ -16,13 +15,12 @@ export const FavScroller = (props) => {
                 if (response.ok) {
                     const data = await response.json();
                     const listOfFavorites = (data.listOfFavorites).map(item => item.movieposterurl);
-                    console.log('List of how many favorites: ',(data.listOfFavorites).length);
                     console.log(listOfFavorites);
-                    if ((listOfFavorites).length < 4) {
-                        for (let i = 0; i < (4 - (listOfFavorites).length); i++) {
-                            listOfFavorites.push(placeholder);
-                        }
-                    }
+                    // if ((listOfFavorites).length < 4) {
+                    //     for (let i = 0; i < (4 - (listOfFavorites).length); i++) {
+                    //         listOfFavorites.push(placeholder);
+                    //     }
+                    // }
                     setFavoriteList(listOfFavorites);
     
                 } else console.error('Failed to fetch favorite movies: ', response.statusText);
