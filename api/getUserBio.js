f33ace704602e42c5b22fc30a2efc3ada7204f72
Brandon Quon <bqa7@sfu.ca>
2024-03-14 18:34:56 -0700

    Fixing

--- api/getUserBio.js
+++ api/getUserBio.js
@@ -9,7 +9,7 @@ export default async function handler(request, response) {
         const existingBioResult = await client.sql`SELECT bio FROM userInfo WHERE userID = ${userID};`;
         
         if (existingBioResult.rows.length > 0 ) {
-            existingBio = existingBioResult;
+            existingBio = existingBioResult.rows[0];
         }
 
         const watchlistResult = await client.sql`SELECT movieposterURL FROM watchlist WHERE userID = ${userID};`;
