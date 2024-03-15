75629abd1587f7b597c81ec963f7ee8cfee72864
Brandon Quon <bqa7@sfu.ca>
2024-03-15 09:22:08 -0700

    navigates but only from movie page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -57,7 +57,9 @@ export const Header = (props) => {
           if (response.ok) {
             const data = await response.json();
             console.log(data);
-            navigate('/user', { state: { data } });
+            if (data) {
+              navigate('/user', { state: { data } });
+            }
           } else console.error('Failed to fetch search results: ', response.statusText); 
 
         } catch (error) {
