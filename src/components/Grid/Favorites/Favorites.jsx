e8fcf9f02a7a67871e89a6096e2fcea1c5ec5eb1
Brandon Quon <bqa7@sfu.ca>
2024-03-17 00:44:13 -0700

    Working on favorites

--- src/components/Grid/Favorites/Favorites.jsx
+++ src/components/Grid/Favorites/Favorites.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import "./Favorites.css"
 
 export const Favorites = () => {
   return (
-    <div id='random'>Favorites</div>
+    <div id="favoritesContainer">
+      <div id="favoritesTitle">
+        <span><FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",}} /><h1> Favorites</h1></span>
+      </div>
+    </div>
   )
 }
