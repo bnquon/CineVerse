37546d3ed6ff19b51f752837fdab99bec7227485
Brandon Quon <bqa7@sfu.ca>
2024-03-17 13:03:00 -0700

    Error

--- src/components/Grid/Favorites/Favorites.jsx
+++ src/components/Grid/Favorites/Favorites.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useInView } from 'react-intersection-observer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
