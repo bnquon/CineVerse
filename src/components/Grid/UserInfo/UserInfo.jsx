7d7ea86105cd56afa01537b7fbe7b49eb2c39f1f
Brandon Quon <bqa7@sfu.ca>
2024-03-18 13:17:30 -0700

    Changing

--- src/components/Grid/UserInfo/UserInfo.jsx
+++ src/components/Grid/UserInfo/UserInfo.jsx
@@ -64,7 +64,7 @@ export const UserInfo = (props) => {
           <textarea
           placeholder="User has no bio"
           value={bioValue}
-          disabled
+          readOnly
           onClick={(e) => e.preventDefault()}
           ></textarea>
           :
