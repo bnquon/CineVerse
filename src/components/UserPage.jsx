61d0d9ac504b9dbe4992e2dc2e5ccc6ad9f2104e
Brandon Quon <bqa7@sfu.ca>
2024-03-18 13:13:50 -0700

    Trying to rerender grid on search

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Header } from "./Header/Header.jsx";
 import { Grid } from "./Grid/Grid.jsx";
 import { useLocation } from 'react-router-dom';
 
 export const UserPage = () => {
 
-
   const location = useLocation();
   const searchedUserData = location.state?.data;
   console.log('SEARCHEDUSERDATA IS', searchedUserData);
 
+  const [key, setKey] = useState(0);
   const storedUsername = sessionStorage.getItem('username');
+
+  useEffect(() => {
+    if (searchedUserData) {
+      setKey(searchedUserData.userID);
+    }
+  }, [searchedUserData]);
 
   return (
     <>         
         <Header username={storedUsername}/>
-        <Grid username = {(searchedUserData).username} userID = {(searchedUserData).userID}/> 
+        <div key={key}>
+          <Grid username = {(searchedUserData).username} userID = {(searchedUserData).userID}/> 
+        </div>
     </>
   )
 }
