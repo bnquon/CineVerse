b60cf0b65c951d608ce20e2e5efbd649e0d47fc6
Brandon Quon <bqa7@sfu.ca>
2024-03-18 23:39:16 -0700

    Fixing infinite rerender

--- src/components/Grid/UserInfo/NoWatchlist/NoWatchlist.jsx
+++ src/components/Grid/UserInfo/NoWatchlist/NoWatchlist.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './NoWatchlist.css'
 
 export const NoWatchlist = (props) => {
 
   const [watchlistMsg, setwatchlistMsg] = useState('');
-  if (props.isUsersPage) {
-    setwatchlistMsg('Save a movie to your watchlist!');
-  } else {
-    setwatchlistMsg('User has no watchlist');
-  }
+  useEffect(() => {
+      if (props.isUsersPage) {
+        setwatchlistMsg('Save a movie to your watchlist!');
+      } else {
+        setwatchlistMsg('User has no watchlist');
+      }
+  }, [props])
 
   return (
     <div id='noWatchList'>
