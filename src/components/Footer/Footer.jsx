d66390a09b0a880fa083d676f8f4932744b8f9d7
Brandon Quon <bqa7@sfu.ca>
2024-03-20 10:14:05 -0700

    Seeing how footer looks

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./Footer.css"
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faGithub } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export const Footer = () => {
   return (
-    <div id='footer'>
-      My Links
-      
+    <div id='footer'> 
+      <span>
+        <FontAwesomeIcon icon={faGithub}/>
+        <FontAwesomeIcon icon={faLinkedin}/>
+      </span>
     </div>
   )
 }
