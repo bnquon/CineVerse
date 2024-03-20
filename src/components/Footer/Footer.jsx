707e5232aea720525ea4644a1027cb7d1df2e368
Brandon Quon <bqa7@sfu.ca>
2024-03-20 10:22:46 -0700

    Adjustinf margin

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,9 @@ import "./Footer.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
+//www.linkedin.com/in/brandon-quon
+//https://github.com/bnquon
+
 export const Footer = () => {
   return (
     <div id='footer'> 
